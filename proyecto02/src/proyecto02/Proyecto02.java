7a2ca5fa13c6d67e952171f5ec56072caddf74a5
GIT-gezzy <jdavichomaldonado@gmail.com>
2025-10-16 11:39:54 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author jdavi
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java "
+                + "Versionado a través de GIT "
+                + "Manejado desde GitHub"
+                + "Además se usar GitHubClassroom");// TODO code application logic here
+    }
+    
+}
